c8de44c181412402827ab1906b1f48f69a62b559
Willy Ovalle <willyovalle16@gmail.com>
2020-06-08 01:01:03 +0200

    feat: class until jun 07

---
+++ calc.py
@@ -0,0 +1,4 @@
+a = int(input('Ingrese el primer digito'))
+b = int(input('Ingrese el segundo digito'))
+
+print('La suma es ', a * b)
---
+++ for.py
@@ -0,0 +1,26 @@
+# Escriba un programa que me devuelva la suma hasta n
+
+# Usuario ingresa 5 (1,2,3,4,5)
+# Imprime: Suma: 15
+
+num = int(input('Ingrese n: ')) # 5
+result = 0
+
+for i in range(num + 1):
+  result = result + i
+
+print('La sumatoria hasta n de ', num, ' es igual a: ', result)
+
+
+# Escriba un programa que me devuelva la suma hasta n de los numeros pares y de los numeros impares
+
+# Usuario ingresa 5
+# Imprime: Suma números pares: 6
+# Imprime: Suma números impares: 9
+
+
+# Calculadora de edad con podere
+# Ingrese fecha actual: 08/06/2020
+# Ingrese fecha de nacimiento: 16/11/1992
+# Tu tienes: x años, y meses, z días
+
