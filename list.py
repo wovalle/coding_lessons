9e214a452e1a0d576bd4dd6fb9d7ae9efd89e93c
Willy Ovalle <willyovalle16@gmail.com>
2020-08-02 23:54:52 +0200

    feat: adding string and list examples

---
+++ list.py
@@ -0,0 +1,11 @@
+pokemons = ["bulbasaur", "squirtle", "charmander"]
+
+pokemons.append('pikachu')
+print("with append", pokemons)
+
+pokemons.insert(0, 'Willy')
+print("with insert", pokemons)
+
+pokemons.reverse()
+print("reverse", pokemons)
+
